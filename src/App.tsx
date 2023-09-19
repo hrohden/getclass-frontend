210990c9a22a9ad067dededd7de1440747337b31
Henrique Broering Rohden <hrohden@gmail.com>
2023-09-19 18:53:14 -0300

    Adds react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,17 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import MomentCard from "./MomentCard";
-import { Moment } from "./Moment";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const [moments] = useState<Moment[]>([
-    {
-      title: "Trip to NYC",
-      headline: "My trip to NY with my wife",
-      description:
-        "It was an amazing trip! Many places to take pictures and visit!",
-      isFavorite: false,
-    },
-    {
-      title: "Trip to Europe",
-      headline: "My trip to Europe with my wife in 2014",
-      description:
-        "This trip was when we were waiting for Lucas. Many pictures and great moments.",
-      isFavorite: false,
-    },
-  ]);
-
   return (
     <>
       <Navbar />
-      <main className="container mx-auto">
-        <h1 className="text-6xl font-bold tracking-tight pb-4">All moments</h1>
-        <div className="space-y-0.5">
-          <p className="text-muted-foreground">
-          Here you can find all important moments in my life
-          </p>
-        </div>
-        {moments.length === 0 && (
-          <p>Currently, there are no moments to display</p>
-        )}
-        {moments.map((moment) => (
-          <MomentCard moment={moment}></MomentCard>
-        ))}
-      </main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
