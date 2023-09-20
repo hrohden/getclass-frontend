7823eda3cb3bd8263b3e0e8e4da490653edd373c
Henrique Broering Rohden <hrohden@gmail.com>
2023-09-20 10:47:51 -0300

    Fix CSS in navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="w-full mb-4 bg-gray-800">
-      <div className="container pt-4 pb-4 mx-auto flex gap-4">
-        <h2 className="text-xl font-medium text-gray-300"><Link to="/">Moments</Link></h2>
-        <ul className="list-none flex">
-          <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><Link to={'/'}>All moments</Link></li>
-          <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><Link to={'/new'}>New moment</Link></li>
-          <li className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><Link to={'/favorites'}>Favorites</Link></li>
-        </ul>
+      <div className="container flex py-1">
+          <h2 className="text-xl font-medium flex items-center pr-3"><Link className="text-gray-300 hover:text-white" to="/">Moments</Link></h2>
+          <ul className="flex">
+            <li className="text-sm font-medium flex items-stretch"><Link className="text-gray-300 hover:bg-gray-700 hover:text-white flex items-center px-3 py-3" to={'/'}>All moments</Link></li>
+            <li className="text-sm font-medium flex items-stretch"><Link className="text-gray-300 hover:bg-gray-700 hover:text-white flex items-center px-3 py-3" to={'/new'}>New moment</Link></li>
+            <li className="text-sm font-medium flex items-stretch"><Link className="text-gray-300 hover:bg-gray-700 hover:text-white flex items-center px-3 py-3" to={'/favorites'}>Favorites</Link></li>
+          </ul>
       </div>
     </nav>
   );
