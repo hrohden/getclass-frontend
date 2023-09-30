4f29a0405da926043fba18ee3c28e671412c3bde
Henrique Broering Rohden <hrohden@gmail.com>
2023-09-30 17:40:15 -0300

    Creates content to Edit page

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import MomentForm from '../components/MomentForm'
+import { useGetMoments } from '../hooks/useMoment'
 import DefaultLayout from '../layouts/DefaultLayout'
+import { Moment } from '../types/Moment'
 
 const Edit = () => {
+  const { id } = useParams()
+  const [moment, setMoment] = useState<Moment>()
+  const getMoments = useGetMoments()
+  useEffect(() => {
+    setMoment(getMoments.data?.find(m => m.id === id))
+  }, [])
   return (
     <DefaultLayout
       title='Edit moment'
       description='Change your moment information here.'
     >
-      Test
+      <MomentForm moment={moment!} />
     </DefaultLayout>
   )
 }
