51cce77c8701f27c06f36aac7b81b777ab46613f
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-02 13:12:05 -0300

    Change Edit form to get the moment by id

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,19 +1,22 @@
+import { useParams } from 'react-router-dom'
 import MomentForm from '../components/MomentForm'
+import { useGetMoment } from '../hooks/useMoment'
 import DefaultLayout from '../layouts/DefaultLayout'
 
 const Edit = () => {
-  const moment = {
-    title: 'Edit moment',
-    headline: 'Edit moment',
-    description: 'Edit moment',
-    isFavorite: false,
+  const { id } = useParams()
+  const { data, isLoading } = useGetMoment(id!)
+
+  if (isLoading) {
+    return <p>Loading...</p>
   }
+
   return (
     <DefaultLayout
-      title='Edit moment'
-      description='Change your moment information here.'
+      title='New moment'
+      description='Wanna create a new moment to never forget it anymore?'
     >
-      <MomentForm moment={moment} />
+      <MomentForm moment={data!}></MomentForm>
     </DefaultLayout>
   )
 }
