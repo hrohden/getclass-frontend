587d3f520561f6d56ef2c132a22d3747c8f9b0fa
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-02 12:44:45 -0300

    Refactor code to remove unnecessary constant

--- src/hooks/useMoment.tsx
+++ src/hooks/useMoment.tsx
@@ -2,20 +2,18 @@ import { useMutation, useQuery, useQueryClient } from 'react-query'
 import { deleteMoment, getMoments, postMoment } from '../api/moment'
 import { Moment } from '../types/Moment'
 
-const key = 'moments'
-
 const useGetMoments = () => {
-  return useQuery([key], getMoments)
+  return useQuery(['moments'], getMoments)
 }
 
 const useCreateMoment = () => {
   const queryClient = useQueryClient()
   return useMutation(postMoment, {
     onSuccess: (moment: Moment) => {
-      queryClient.setQueryData([key], (old: Moment[] | undefined) => {
+      queryClient.setQueryData(['moments'], (old: Moment[] | undefined) => {
         return old ? [...old, moment] : [moment]
       })
-      queryClient.invalidateQueries([key])
+      queryClient.invalidateQueries(['moments'])
     },
   })
 }
@@ -25,10 +23,10 @@ const useDeleteMoment = () => {
   const queryClient = useQueryClient()
   return useMutation(deleteMoment, {
     onSuccess: (id: string) => {
-      queryClient.setQueryData([key], (old: Moment[] | undefined) => {
+      queryClient.setQueryData(['moments'], (old: Moment[] | undefined) => {
         return old ? old.filter(m => m.id !== id) : []
       })
-      queryClient.invalidateQueries([key])
+      queryClient.invalidateQueries(['moments'])
     },
   })
 }
--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,23 +1,19 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
 import MomentForm from '../components/MomentForm'
-import { useGetMoments } from '../hooks/useMoment'
 import DefaultLayout from '../layouts/DefaultLayout'
-import { Moment } from '../types/Moment'
 
 const Edit = () => {
-  const { id } = useParams()
-  const [moment, setMoment] = useState<Moment>()
-  const getMoments = useGetMoments()
-  useEffect(() => {
-    setMoment(getMoments.data?.find(m => m.id === id))
-  }, [])
+  const moment = {
+    title: 'Edit moment',
+    headline: 'Edit moment',
+    description: 'Edit moment',
+    isFavorite: false,
+  }
   return (
     <DefaultLayout
       title='Edit moment'
       description='Change your moment information here.'
     >
-      <MomentForm moment={moment!} />
+      <MomentForm moment={moment} />
     </DefaultLayout>
   )
 }
