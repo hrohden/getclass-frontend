9349dba49dac49dda2d44fe1de4a14ea7a07e13c
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-19 15:24:20 -0300

    Define configuration for the dev command

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,24 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // configure CORS proxy
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      // configure CORS proxy
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            rewrite: path => path.replace(/^\/api/, ''),
+          },
+        },
       },
-    },
-  },
+    }
+  } else {
+    return {
+      plugins: [react()],
+    }
+  }
 })
