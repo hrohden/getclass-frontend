03da89bcc49a1a75b3f7c204df235a47e4a363fb
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-23 11:39:14 -0300

    Restore CORS configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,18 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  if (command === 'serve') {
-    return {
-      plugins: [react()],
-      // configure CORS proxy
-      server: {
-        proxy: {
-          '/api': {
-            target: 'http://localhost:3000',
-            changeOrigin: true,
-            rewrite: path => path.replace(/^\/api/, ''),
-          },
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    // configure CORS proxy
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
         },
       },
-    }
-  } else {
-    return {
-      plugins: [react()],
-    }
+    },
   }
 })
