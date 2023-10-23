2d24abd98c4c370be61baf50bcb035706a927b23
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-23 13:43:33 -0300

    Adds functions to get data asynchronously

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,11 +1,31 @@
+import { Button } from 'flowbite-react'
+import { useDispatch, useSelector } from 'react-redux'
 import DefaultLayout from '../layouts/DefaultLayout'
+import { clean, getData } from '../store/momentsSlice'
+import { AppStore } from '../types/AppStore'
+import { Moment } from '../types/Moment'
 
 const Favorites = () => {
+  const selector = useSelector<AppStore, Moment[]>(state => state.moments)
+  const dispatch = useDispatch()
+
+  const handleGetData = () => {
+    // @ts-ignore
+    dispatch(getData())
+  }
+
+  const handleCleanData = () => {
+    dispatch(clean([]))
+  }
   return (
     <DefaultLayout
       title='Favorites'
       description='Keep track of the most favorites moments of my life'
-    ></DefaultLayout>
+    >
+      <p>{selector.length}</p>
+      <Button onClick={handleGetData}>Get async data from API</Button>
+      <Button onClick={handleCleanData}>Clean data</Button>
+    </DefaultLayout>
   )
 }
 export default Favorites
--- src/store/momentsSlice.ts
+++ src/store/momentsSlice.ts
@@ -1,4 +1,10 @@
-import { SliceCaseReducers, createSlice } from '@reduxjs/toolkit'
+import {
+  AnyAction,
+  Dispatch,
+  SliceCaseReducers,
+  createSlice,
+} from '@reduxjs/toolkit'
+import { getMoments } from '../api/moment'
 import { Moment } from '../types/Moment'
 
 const momentsSlice = createSlice<Moment[], SliceCaseReducers<Moment[]>>({
@@ -9,7 +15,7 @@ const momentsSlice = createSlice<Moment[], SliceCaseReducers<Moment[]>>({
       const newState = [...prevState, payload]
       return newState
     },
-    addAll: (_prevState, { payload }) => {
+    set: (_prevState, { payload }) => {
       return payload
     },
     clean: () => {
@@ -18,5 +24,18 @@ const momentsSlice = createSlice<Moment[], SliceCaseReducers<Moment[]>>({
   },
 })
 
-export const { add, clean, addAll } = momentsSlice.actions
+export const getData = () => {
+  return async (dispatch: Dispatch<AnyAction>) => {
+    try {
+      // make an async call in the thunk
+      const moments = await getMoments()
+      // dispatch an action when we get the response back
+      dispatch(set(moments))
+    } catch (err) {
+      // If something went wrong, handle it here
+    }
+  }
+}
+
+export const { add, clean, set } = momentsSlice.actions
 export default momentsSlice.reducer
