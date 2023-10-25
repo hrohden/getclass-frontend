444f17d3f5a3ae9ed67cb636b62ffb360d084df6
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-25 14:53:03 -0300

    Sorting ascending and descending

--- src/components/TableMoments.tsx
+++ src/components/TableMoments.tsx
@@ -1,4 +1,4 @@
-import { Button, Table } from 'flowbite-react'
+import { Button, Checkbox, Label, Select, Table } from 'flowbite-react'
 import { useState } from 'react'
 import toast from 'react-hot-toast'
 import { useDispatch } from 'react-redux'
@@ -9,13 +9,18 @@ import NoMomentsToDisplayAlert from './NoMomentsToDisplayAlert'
 
 const TableMoments = ({ moments }: { moments: Moment[] | undefined }) => {
   const [listMoments, setListMoments] = useState<Moment[] | undefined>(moments)
+  const [criteria, setCriteria] = useState('title')
+  const [asc, setAsc] = useState(false)
   const dispatch = useDispatch()
-  const handleSortByTitle = () => {
+  const handleSort = () => {
     setListMoments(prevState => {
       let newState = prevState ? [...prevState] : []
+      let ascOrder = asc ? 1 : -1
       newState.sort((a, b) => {
-        if (a.title > b.title) return 1
-        if (a.title < b.title) return -1
+        // @ts-ignore
+        if (a[criteria] > b[criteria]) return 1 * ascOrder
+        // @ts-ignore
+        if (a[criteria] < b[criteria]) return -1 * ascOrder
         return 0
       })
       return newState
@@ -25,19 +30,35 @@ const TableMoments = ({ moments }: { moments: Moment[] | undefined }) => {
     <NoMomentsToDisplayAlert />
   ) : (
     <>
-      <div className='mb-4 flex gap-4'>
-        <Button onClick={handleSortByTitle}>Sort by title</Button>
+      <div className='mb-4 flex items-center gap-4'>
+        <Button onClick={handleSort}>Sort</Button>
+        <Select onChange={e => setCriteria(e.target.value)}>
+          <option value='title'>title</option>
+          <option value='description'>description</option>
+          <option value='headline'>headline</option>
+        </Select>
+        <Checkbox
+          id='asc'
+          onChange={() => setAsc(!asc)}
+          value='asc'
+          checked={asc}
+        />
+        <Label htmlFor='asc'>
+          <p>Ascending</p>
+        </Label>
       </div>
       <Table striped>
         <Table.Head>
           <Table.HeadCell>Title</Table.HeadCell>
+          <Table.HeadCell>Headline</Table.HeadCell>
           <Table.HeadCell>Description</Table.HeadCell>
           <Table.HeadCell>Actions</Table.HeadCell>
         </Table.Head>
         <Table.Body>
-          {listMoments?.map(({ id, title, description }) => (
+          {listMoments?.map(({ id, title, headline, description }) => (
             <Table.Row key={id}>
               <Table.Cell>{title}</Table.Cell>
+              <Table.Cell>{headline}</Table.Cell>
               <Table.Cell>{description}</Table.Cell>
               <Table.Cell className='flex gap-4'>
                 <Link to={`/edit/${id}`}>Edit</Link>
