f21c6c0f973d7db72344a9bad4bc70b1732f0d36
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-25 13:11:59 -0300

    Refactor Edit page component to use Redux thunk for fetching moment data.

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,33 +1,41 @@
+import { useEffect } from 'react'
 import toast from 'react-hot-toast'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
 import MomentForm from '../components/MomentForm'
-import { useGetMoment, useUpdateMoment } from '../hooks/useMoment'
 import DefaultLayout from '../layouts/DefaultLayout'
+import { selectMomentByIdThunk } from '../store/momentSlice'
+import { AppStore } from '../types/AppStore'
 import { Moment } from '../types/Moment'
 
 const Edit = () => {
-  const updateMoment = useUpdateMoment()
   const { id } = useParams()
-  const { data, status } = useGetMoment(id!)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { data, status } = useSelector((state: AppStore) => state.moment)
 
-  if (status) {
-    return <p>Loading...</p>
-  }
+  useEffect(() => {
+    // @ts-ignore
+    dispatch(selectMomentByIdThunk(id))
+  }, [])
 
   return (
     <DefaultLayout
       title='Edit moment'
       description='Wanna create a new moment to never forget it anymore?'
     >
-      <MomentForm
-        moment={data!}
-        submitForm={async (moment: Moment) => {
-          await updateMoment.mutateAsync(moment)
-          toast.success('Moment changed successfully!')
-          navigate('/')
-        }}
-      ></MomentForm>
+      {status === 'loading' ? (
+        <p>Loading...</p>
+      ) : (
+        <MomentForm
+          moment={data!}
+          submitForm={async (moment: Moment) => {
+            // await updateMoment.mutateAsync(moment)
+            toast.success('Moment changed successfully!')
+            navigate('/')
+          }}
+        ></MomentForm>
+      )}
     </DefaultLayout>
   )
 }
