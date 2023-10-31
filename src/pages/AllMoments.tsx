6daf3dc8dac6c94fcc3e1f464306880ade226ad2
Henrique Broering Rohden <hrohden@gmail.com>
2023-10-31 16:25:55 -0300

    Add useEffect and useSelector hooks to fetch and display moments data

--- src/pages/AllMoments.tsx
+++ src/pages/AllMoments.tsx
@@ -1,15 +1,30 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import TableMoments from '../components/TableMoments'
 import DefaultLayout from '../layouts/DefaultLayout'
-import fakeMoments from '../store/fakeMoments'
+import { fetchMoments } from '../store/momentsSlice'
+import { AppStore } from '../types/AppStore'
 
 const AllMoments = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    // @ts-ignore
+    dispatch(fetchMoments())
+  }, [])
+
+  const { data, status } = useSelector((state: AppStore) => state.moments)
+
   return (
     <DefaultLayout
       title='All moments'
       documentTitle='Moments'
       description='Here you can find all important moments in my life'
     >
-      <TableMoments moments={fakeMoments} />
+      {status === 'loading' || status === 'idle' ? (
+        <p>Loading...</p>
+      ) : (
+        <TableMoments moments={data} />
+      )}
     </DefaultLayout>
   )
 }
